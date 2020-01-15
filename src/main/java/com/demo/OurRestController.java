fb21312c766d58f92fb6ceed32db95d0cdf5bc4e
Daniel e Kowalik <daniel@kowalik.org.pl>
2020-01-15 10:20:44 +0100

    Part 2

--- src/main/java/com/demo/OurRestController.java
+++ src/main/java/com/demo/OurRestController.java
@@ -1,5 +1,8 @@
 package com.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OurRestController {
 
+
+    private SampleService sampleService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private SampleService sampleServiceBean;
+
+    @Autowired
+    public OurRestController(SampleService sampleService) {
+        this.sampleService=sampleService;
+    }
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String helloWorld() {
+        
+        System.out.println(sampleService.whatever());
+        System.out.println(sampleServiceBean.whatever());
         return "Hello World";
     }
+
+
 }
---
+++ src/main/java/com/demo/SampleService.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleService {
+
+    public String whatever(){
+        return "WHATEVER";
+    }
+}
